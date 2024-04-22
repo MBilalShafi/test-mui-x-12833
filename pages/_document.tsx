0d0a03cc2bd42ebc7104b381a5aa6108fa147c41
Bilal Shafi <bilalshafidev@gmail.com>
2024-04-23 01:59:06 +0500

    Test mui/mui-x#12833

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({});
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <ThemeProvider theme={theme}>
+      <Html lang="en">
+        <Head />
+        <body>
+          {/* <Main /> */}
+          <NextScript />
+        </body>
+      </Html>
+    </ThemeProvider>
   );
 }
